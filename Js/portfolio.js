89495ae51ff7bb13721b38b0b14901c40f24f328
Akshesh88 <pakshesh88@gmail.com>
2023-09-23 20:08:42 -0400

    Added image

---
+++ Js/portfolio.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.element',{
+  strings: ['Web designer', 'Frontend developer', 'Web developer'],
+  typeSpeed: 100,
+  backspeed: 100,
+  backdelay: 1000,
+  loop: true
+});
+
+var typed = new Typed('.element2',{
+  strings: ['Web designer', 'Frontend developer', 'Web developer'],
+  typeSpeed: 100,
+  backspeed: 100,
+  backdelay: 1000,
+  loop: true
+});
